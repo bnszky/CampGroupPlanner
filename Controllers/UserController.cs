2001c14829aacf4668ec0a8843963d8bc7d76292
bnszky <michal.banaszkiewicz.pvt@gmail.com>
2024-02-22 19:28:03 +0100

    Create page validation

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using CampGroupPlanner.Data;
 using CampGroupPlanner.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Text.RegularExpressions;
 
 namespace CampGroupPlanner.Controllers
 {
@@ -24,9 +26,20 @@ namespace CampGroupPlanner.Controllers
         [HttpPost]
         public IActionResult Create(User user)
         {
-            _db.Users.Add(user);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+			if(user.Email == null || !Regex.IsMatch(user.Email, "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"))
+            {
+                ModelState.AddModelError("Email", "Email is invalid!");
+            }
+			if (ModelState.IsValid)
+            {
+                _db.Users.Add(user);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -5,11 +5,13 @@ namespace CampGroupPlanner.Models
     public class User
     {
         public int Id { get; set; }
+        [MaxLength(25)]
         public string Nick { get; set; } = string.Empty;
 
         [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
        
     }
